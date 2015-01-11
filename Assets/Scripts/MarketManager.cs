3b747ac5e276b1cee9a7c503b0e1b36832a035dc
Buda <thatbuddhadude@gmail.com>
2015-01-11 02:55:36 -0600

    +Added Market/Upgrade Layout +Can add items to upgrade...kinda +Item displays change with item stuff +Items can be selected +New Graphics for the Upgrades! +Animation stuff

--- Assets/Scripts/MarketManager.cs
+++ Assets/Scripts/MarketManager.cs
@@ -1,10 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class MarketManager : MonoBehaviour {
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class MarketManager : MonoBehaviour
+{
 	public bool fridge;
 
+    public Text displayName;
+    public Text displayDesc;
+    public Text displayCost;
+    public Image displayImage;
 
+    public UpgradeItem upgradeItemSlot;
+    public GameObject parentObject;
+
+    public UpgradeItem selected;
+
+    public List<UpgradeItem> upgradeItems;
 
 	public void BuyFridge(){
 
@@ -12,11 +26,48 @@ public class MarketManager : MonoBehaviour {
 		}
 	// Use this for initialization
 	void Start () {
-	
+        NewItem("Small Freezer", "This fridge is your basic 2 bedroom apartment fridge. We had to remove the compressor to cut costs but it keeps your milk cold with the power hopes and dreams", 700);
+        NewItem("Large Fridge", "This isn't your grandma's fridge. This fridge runs on pure liquid nitrogen. It can fit up 5 human bodies. Not that we recommend that!", 1500);
+
+        NewItem("Cow", "Butt", 5200);
+        NewItem("Cocoa Plant", "Butt", 500);
+        NewItem("Mixer", "Butt", 3500);
+        NewItem("Null", "Butt", 1500);
+        NewItem("None", "Butt", 22500);
+
+        selected = upgradeItems[0];
+
+        
 	}
+
+    public void NewItem(string name, string desc, float cost)
+    {
+        UpgradeItem itemScript = (UpgradeItem)Instantiate(upgradeItemSlot);
+        itemScript.itemName = name;
+        itemScript.itemDesc = desc;
+        itemScript.itemCost = cost;
+        itemScript.transform.SetParent(parentObject.transform);
+        upgradeItems.Add(itemScript);
+    }
+
+    public void RefreshDisplay(UpgradeItem item)
+    {
+        selected = item;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+       
+
+        if (selected != null)
+        {
+            displayName.text = selected.itemName;
+            displayDesc.text = selected.itemDesc;
+            displayCost.text = "Cost: "+selected.itemCost;
+            displayImage.sprite = selected.itemIcon;
+            
+        }
+
 	}
 }
---
+++ Assets/UpgradeItem.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class UpgradeItem : MonoBehaviour {
+
+    public string itemName;
+    public string itemDesc;
+    public float itemCost;
+    public Sprite itemIcon;
+    private Text displayName;
+    private Button button;
+
+    private bool purchased;
+
+    private MarketManager mManager;
+    private Image itemImage;
+
+    public UpgradeItem(string newName, string newDesc, float newCost)
+    {
+        itemName = newName;
+        itemDesc = newDesc;
+        itemCost = newCost;
+    }
+
+	// Use this for initialization
+	void Start () {
+        displayName = gameObject.GetComponentInChildren<Text>();
+        button = gameObject.GetComponent<Button>();
+        mManager = GameObject.FindObjectOfType<MarketManager>();
+        itemImage = gameObject.GetComponentInChildren<Image>();
+        button.onClick.AddListener(()=>MakeSelected());
+
+
+        itemIcon = Resources.Load<Sprite>("Upgrades/" + itemName);
+        itemImage.sprite = itemIcon;
+	}
+
+    void MakeSelected()
+    {
+        mManager.RefreshDisplay(this);
+    }
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+        displayName.text = itemName;
+
+        
+	}
+}
